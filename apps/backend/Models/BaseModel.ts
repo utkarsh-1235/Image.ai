7645a245bfe94a31ad1e98992967eb919f19bcdf
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2025-04-07 23:11:45 +0530

    Adding trainModel with fal-ai in Backend Models folder

---
+++ apps/backend/Models/BaseModel.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+export class BaseModel{
+    constructor(){}
+
+    public async generateImage(prompt: string){
+
+    }
+
+    public async trainModel(prompt: string){
+        
+    }
+}
